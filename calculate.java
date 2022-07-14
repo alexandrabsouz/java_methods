791804e2bd7d9c04e1a01f6cf0080d835565e42b
Alexandra Souza <alexandrabsouz@gmail.com>
2022-07-14 09:00:37 -0300

    first commit

---
+++ calculate.java
@@ -0,0 +1,11 @@
+package java_methods;
+
+public class calculate {
+    public static void soma(double numero1, double numero2) {
+
+        double resultado = numero1 + numero2;
+
+        System.out.println("A soma de " + numero1 + " + " + numero2 + "é " + resultado);
+
+    }
+}
