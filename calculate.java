23b4d829e8b837132349c3ae5fbc0bcddc21e622
Alexandra Souza <alexandrabsouz@gmail.com>
2022-07-14 09:03:03 -0300

    chore: div calculate function

--- calculate.java
+++ calculate.java
@@ -21,7 +21,15 @@ public class calculate {
 
         double resultado = numero1 * numero2;
 
-        System.out.println("A mulçtiplicação de " + numero1 + " + " + numero2 + "é " + resultado);
+        System.out.println("A multiplicação de " + numero1 + " + " + numero2 + "é " + resultado);
+
+    }
+
+    public static void div(double numero1, double numero2) {
+
+        double resultado = numero1 / numero2;
+
+        System.out.println("A divisão de " + numero1 + " + " + numero2 + "é " + resultado);
 
     }
 }
