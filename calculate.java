902b041c4bd46ed1406a1a5787166389e59daf1b
Alexandra Souza <alexandrabsouz@gmail.com>
2022-07-14 09:01:25 -0300

    chore: sub calculate function

--- calculate.java
+++ calculate.java
@@ -8,4 +8,12 @@ public class calculate {
         System.out.println("A soma de " + numero1 + " + " + numero2 + "é " + resultado);
 
     }
+
+    public static void sub(double numero1, double numero2) {
+
+        double resultado = numero1 + numero2;
+
+        System.out.println("A subtração de " + numero1 + " + " + numero2 + "é " + resultado);
+
+    }
 }
